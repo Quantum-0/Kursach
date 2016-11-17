ebbaa5d277fd23278603de8fd8ffdf334e21a411
Quantum0 <quantum0@mail.ru>
2016-11-17 21:24:13 +0400

    Ну-с, начнём-с. Пока CUDA не работает и как устроено суффиксное дерево я не понял - сделал решение в лоб :D

---
+++ SuffixTree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuffixTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new NotSuffixTree();
+            tree.Increment("A");
+            tree.Increment("Apple");
+            tree.Increment("Banana");
+            tree.Increment("Bananas");
+            tree.Increment("Banana");
+            tree.Increment("Fruit");
+
+            using (StreamReader sr = new StreamReader("F:\\test.txt", Encoding.Unicode))
+            {
+                var Input = sr.ReadToEnd();
+                var Words = Input.Split(new char[] { ' ', ',', '.', ':', '?', '!', '\n' });
+                foreach (var Word in Words)
+                {
+                    tree.Increment(Word);
+                }
+            }
+
+            var s = tree.Export();
+            s.Sort();
+
+            using (StreamWriter sw = new StreamWriter("TestResult.txt"))
+            {
+                foreach (var item in s)
+                {
+                    sw.WriteLine(item.Word + " - " + item.Count);
+                }
+            }
+        }
+    }
+}
---
+++ SuffixTree/SuffixTree.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuffixTree
+{
+    struct WordCountPair : IComparable
+    {
+        public string Word;
+        public long Count;
+
+        public int CompareTo(object obj)
+        {
+            if (obj is WordCountPair)
+                return -Count.CompareTo(((WordCountPair)obj).Count);
+            else
+                throw new InvalidOperationException();
+        }
+    }
+
+    class NotSuffixTree
+    {
+        Node Root;
+        class Node
+        {
+            public char Char;
+            public long Count;
+            public List<Node> Childs;
+
+            public Node(Char Char)
+            {
+                this.Char = Char;
+                Count = 0;
+                Childs = new List<Node>();
+            }
+        }
+
+        public NotSuffixTree()
+        {
+            Root = new Node('\0');
+        }
+
+        public void Increment(string Word)
+        {
+            Node Current = Root;
+            var NotInitialized = false;
+
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (NotInitialized)
+                {
+                    var New = new Node(Word[i]);
+                    Current.Childs.Add(New);
+                    Current = New;
+                }
+                else
+                {
+
+                    if (!Current.Childs.Exists(n => n.Char == Word[i]))
+                    {
+                        NotInitialized = true;
+                        var New = new Node(Word[i]);
+                        Current.Childs.Add(New);
+                        Current = New;
+                    }
+                    else
+                    {
+                        Current = Current.Childs.Find(n => n.Char == Word[i]);
+                    }
+                }
+            } // for
+            Current.Count++;
+        }
+
+        public List<WordCountPair> Export()
+        {
+            var Result = new List<WordCountPair>();
+            _Export(Result, String.Empty, Root);
+            return Result;
+        }
+
+        private void _Export(List<WordCountPair> Result, string Word, Node Current)
+        {
+            foreach (var Child in Current.Childs)
+            {
+                if (Child.Count > 0)
+                    Result.Add(new WordCountPair() { Word = Word + Child.Char, Count = Child.Count });
+
+                if (Child.Childs.Count > 0)
+                    _Export(Result, Word + Child.Char, Child);
+            }
+        }
+    }
+}
