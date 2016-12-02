998839f07eca1ea87d0c50c7692d9b5855bdb163
Quantum0 <quantum0@mail.ru>
2016-12-02 20:39:10 +0400

    Надо было просто выбрать третью тему без автомата и всё, было бы уже готово :D

--- SuffixTree/Program.cs
+++ SuffixTree/Program.cs
@@ -3,30 +3,74 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SuffixTree
 {
     class Program
     {
+        static void FileToTree(string path, NotSuffixTree tree)
+        {
+            var Word = new StringBuilder();
+            var WordsFound = 0;
+            var Percents = 0;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Обработка файла ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Path.GetFileName(path));
+            Console.Write(" (" + new FileInfo(path).Length/1024 + " Kb)");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("\nТекущий размер словаря повторений слов: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(tree.DifferentWords);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("\nОбработка: [                    ]");
+            Console.CursorLeft = 12;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                do
+                {
+                    var c = Convert.ToChar(sr.Read());
+
+                    if (sr.BaseStream.Position >= sr.BaseStream.Length * (Percents + 5) / 100)
+                    {
+                        Percents += 5;
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write(" ");
+                    }
+
+                    if (char.IsLetter(c))
+                    {
+                        Word.Append(char.ToUpper(c));
+                    }
+                    else if (Word.Length != 0)
+                    {
+                        while (tree.QueueLength > 1000)
+                            Thread.Sleep(100);
+
+                        WordsFound++;
+                        tree.Increment(Word.ToString());
+                        Word.Clear();
+                    }
+                }
+                while (!sr.EndOfStream);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorLeft = 0;
+            Console.CursorTop++;
+            Console.WriteLine("Файл обработан, найдено " + WordsFound + " слов");
+            Console.WriteLine("--- Загрузка ОЗУ: " + GC.GetTotalMemory(false) / 1024 + " КБ ---");
+        }
+
         static void Main(string[] args)
         {
             var tree = new NotSuffixTree();
-            tree.Increment("A");
-            tree.Increment("Apple");
-            tree.Increment("Banana");
-            tree.Increment("Bananas");
-            tree.Increment("Banana");
-            tree.Increment("Fruit");
 
-            using (StreamReader sr = new StreamReader("F:\\test.txt", Encoding.Unicode))
+            var Files = Directory.EnumerateFiles(@"F:\Books\");
+            foreach (var File in Files)
             {
-                var Input = sr.ReadToEnd();
-                var Words = Input.Split(new char[] { ' ', ',', '.', ':', '?', '!', '\n' });
-                foreach (var Word in Words)
-                {
-                    tree.Increment(Word);
-                }
+                FileToTree(File, tree);
             }
 
             var s = tree.Export();
@@ -39,6 +83,8 @@ namespace SuffixTree
                     sw.WriteLine(item.Word + " - " + item.Count);
                 }
             }
+            tree.Dispose();
+            Console.ReadKey();
         }
     }
 }
--- SuffixTree/SuffixTree.cs
+++ SuffixTree/SuffixTree.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace SuffixTree
 {
@@ -18,11 +20,44 @@ namespace SuffixTree
             else
                 throw new InvalidOperationException();
         }
+
+        public override string ToString()
+        {
+            return Word + " - " + Count;
+        }
     }
 
-    class NotSuffixTree
+    class NotSuffixTree : IDisposable
     {
         Node Root;
+        public long DifferentWords;
+        ConcurrentQueue<string> WordsToAdd = new ConcurrentQueue<string>();
+        List<Thread> Adders = new List<Thread>();
+        bool ThreadWorks = true;
+        public int QueueLength { get { return WordsToAdd.Count; } }
+
+        private static void WorkWithWords(object oTree)
+        {
+            NotSuffixTree Tree = (NotSuffixTree)oTree;
+            while (Tree != null)
+            {
+                if (Tree.WordsToAdd.Count > 0)
+                {
+                    Tree.ThreadWorks = true;
+                    while (Tree.WordsToAdd.Count > 0)
+                    {
+                        string Word;
+                        while (!Tree.WordsToAdd.TryDequeue(out Word)) ;
+                        if (Word != null)
+                            Tree._Increment(Word);
+                    }
+                    Tree.ThreadWorks = false;
+                }
+                else
+                    Thread.Sleep(50);
+            }
+        }
+
         class Node
         {
             public char Char;
@@ -35,14 +70,29 @@ namespace SuffixTree
                 Count = 0;
                 Childs = new List<Node>();
             }
+
+            public override string ToString()
+            {
+                return Char + " (" + Childs.Count + ")";
+            }
         }
 
         public NotSuffixTree()
         {
             Root = new Node('\0');
+
+            //for (int i = 0; i < Environment.ProcessorCount - 1; i++)
+                Adders.Add(new Thread(WorkWithWords));
+
+            Adders.ForEach(a => a.Start(this));
         }
 
         public void Increment(string Word)
+        {
+            WordsToAdd.Enqueue(Word);
+        }
+
+        private void _Increment(string Word)
         {
             Node Current = Root;
             var NotInitialized = false;
@@ -70,12 +120,17 @@ namespace SuffixTree
                         Current = Current.Childs.Find(n => n.Char == Word[i]);
                     }
                 }
-            } // for
+            }
+            if (Current.Count == 0)
+                DifferentWords++;
             Current.Count++;
         }
 
         public List<WordCountPair> Export()
         {
+            while (ThreadWorks || !WordsToAdd.IsEmpty)
+                Thread.Sleep(10);
+
             var Result = new List<WordCountPair>();
             _Export(Result, String.Empty, Root);
             return Result;
@@ -91,6 +146,11 @@ namespace SuffixTree
                 if (Child.Childs.Count > 0)
                     _Export(Result, Word + Child.Char, Child);
             }
+        }
+
+        public void Dispose()
+        {
+            Adders.ForEach(a => a.Abort());
         }
     }
 }
